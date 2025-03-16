9d88605e7b4894d6604c18694d00f318f82708c2
MaLiHa1321 <malihasultana2001@gmail.com>
2025-03-16 22:29:09 +0600

    navbar and footer are added

---
+++ src/Component/Errorpage/Errorpage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Errorpage = () => {
+    return (
+        <div>
+            <h2 className='text-5xl text-center'>Page not found</h2>
+            
+        </div>
+    );
+};
+
+export default Errorpage;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+import ReactDOM from "react-dom/client";
+import Root from './Component/root/Root';
+import Errorpage from './Component/Errorpage/Errorpage';
+import Home from './Component/Home/Home';
+import Dashboard from './Component/Dashboard/Dashboard';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <Errorpage />,
+    children:[
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: 'dashboard',
+        element: <Dashboard />
+      }
+
+    ]
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    
+    <RouterProvider router={router} />
+   {/* ReactDOM.createRoot(document.getElementById("root")).render(<App />); */}
   </StrictMode>,
 )
